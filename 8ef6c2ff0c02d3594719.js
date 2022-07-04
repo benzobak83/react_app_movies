965612cf32ea6530c7d75a6790901f19fb29aa09
benzobak83 <vitanaolord@gmail.com>
2022-07-05 01:53:59 +0300

    Updates

--- 8ef6c2ff0c02d3594719.js
+++ 8ef6c2ff0c02d3594719.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from './App.jsx';
-import '../assets/css/main.css';
 import '../assets/scss/main.scss';
-import { Search } from './components/Search.jsx';
 document.addEventListener("DOMContentLoaded", e => {
   ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.querySelector("#root"));
 });
